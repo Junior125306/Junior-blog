5c5c0acf36faf469bafc5934880b4542a826a098
张振宇 <809534382@qq.com>
2020-09-30 21:39:07 +0800

    修改归档页文字👣

--- src/pages/Archives.vue
+++ src/pages/Archives.vue
@@ -1,18 +1,17 @@
 <template>
-  <div :show-logo="false">
-    <h1 class="text-center space-bottom">Archives</h1>
+    <div :show-logo="false">
+        <h1 class="text-center space-bottom">Archives</h1>
 
-    <div class="posts content-box">
-      <p style="opacity: 0.6;">
-        <strong
-          >(｡･∀･)ﾉﾞ I have published a total of {{ totalPosts }} posts,
-          {{ totalWords }} words since 2019.</strong
-        >
-      </p>
+        <div class="posts content-box">
+            <p style="opacity: 0.6;">
+                <strong>
+                    从2020年以来，我总共发布了{{ totalPosts }}篇文章，共{{ totalWords }}字。
+                </strong>
+            </p>
 
-      <div v-for="year in backwardsTimeKey" :key="year">
-        <h5>{{ year }}</h5>
-        <p v-for="p in timeline[year]" :key="p.id">
+            <div v-for="year in backwardsTimeKey" :key="year">
+                <h5>{{ year }}</h5>
+                <p v-for="p in timeline[year]" :key="p.id">
           <span>{{
             new Date(p.date)
               .toLocaleString('en-US', {
@@ -21,83 +20,84 @@
               })
               .replace(' ', '.')
           }}</span
-          ><g-link :to="p.path">{{ p.title }}</g-link>
-        </p>
-      </div>
+          >
+                    <g-link :to="p.path">{{ p.title }}</g-link>
+                </p>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <page-query>
-query {
-  posts: allPost(filter: { published: { eq: true }}) {
+    query {
+    posts: allPost(filter: { published: { eq: true }}) {
     edges {
-      node {
-        id
-        title
-        date
-        cjkWordCount
-        path
-      }
+    node {
+    id
+    title
+    date
+    cjkWordCount
+    path
     }
-  }
-}
+    }
+    }
+    }
 </page-query>
 
 <script>
-export default {
-  metaInfo: {
-    title: 'Archives',
-  },
-  data() {
-    return {
-      timeline: {},
-      backwardsTimeKey: [],
-      totalPosts: 0,
-      totalWords: 0,
+    export default {
+        metaInfo: {
+            title: 'Archives',
+        },
+        data() {
+            return {
+                timeline: {},
+                backwardsTimeKey: [],
+                totalPosts: 0,
+                totalWords: 0,
+            }
+        },
+        methods: {
+            // Covert actual number to `xx.x k` expression, see: https://url.cn/JobLixKT
+            kFormatter: num => {
+                return Math.abs(num) > 999
+                    ? Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + 'k'
+                    : Math.sign(num) * Math.abs(num)
+            },
+        },
+        mounted() {
+            this.totalPosts = this.$page.posts.edges.length
+
+            const timeline = {}
+            this.$page.posts.edges.forEach(post => {
+                this.totalWords += post.node.cjkWordCount
+
+                // Render timeline views
+                const postYear = new Date(post.node.date).getFullYear()
+                if (!(postYear in timeline)) {
+                    timeline[postYear] = []
+                }
+                timeline[postYear].push(post.node)
+            })
+            this.timeline = timeline
+            this.backwardsTimeKey = Object.keys(timeline)
+                .sort()
+                .reverse()
+
+            this.totalWords = this.kFormatter(this.totalWords)
+        },
     }
-  },
-  methods: {
-    // Covert actual number to `xx.x k` expression, see: https://url.cn/JobLixKT
-    kFormatter: num => {
-      return Math.abs(num) > 999
-        ? Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + 'k'
-        : Math.sign(num) * Math.abs(num)
-    },
-  },
-  mounted() {
-    this.totalPosts = this.$page.posts.edges.length
-
-    const timeline = {}
-    this.$page.posts.edges.forEach(post => {
-      this.totalWords += post.node.cjkWordCount
-
-      // Render timeline views
-      const postYear = new Date(post.node.date).getFullYear()
-      if (!(postYear in timeline)) {
-        timeline[postYear] = []
-      }
-      timeline[postYear].push(post.node)
-    })
-    this.timeline = timeline
-    this.backwardsTimeKey = Object.keys(timeline)
-      .sort()
-      .reverse()
-
-    this.totalWords = this.kFormatter(this.totalWords)
-  },
-}
 </script>
 
 <style lang="scss">
-.posts {
-  p span {
-    font-size: 0.85rem;
-    font-family: var(--monospace-font-family);
+    .posts {
+        p span {
+            font-size: 0.85rem;
+            font-family: var(--monospace-font-family);
 
-    &::after {
-      content: ' ';
+            &::after {
+                content: ' ';
+            }
+        }
     }
-  }
-}
 </style>
